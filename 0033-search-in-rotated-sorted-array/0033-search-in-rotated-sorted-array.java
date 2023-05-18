8a436b9898c8ae9861ff230440a3659f6de672e8
Ankita Soni <92040840+aankitasoni@users.noreply.github.com>
2023-05-18 13:03:09 +0530

    Time: 1 ms (6.71%), Space: 42.6 MB (18.15%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        ArrayList<Integer> al = new ArrayList<>();
+        for (int i : nums){
+            al.add(i);
+        }
+        if (al.contains(target)){
+            return al.indexOf(target);
+        } else {
+            return -1;
+        }
+    }
+}
