5491e76e4cdca10e663f23d82bacea5500409f11
Ankita Soni <92040840+aankitasoni@users.noreply.github.com>
2023-05-25 22:36:21 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (10.39%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -8,34 +8,10 @@ class Solution {
                 al.add(i);
             }
         }
-        
         Arrays.fill(nums, 0);
         for (int i = 0; i < al.size(); i++){
             nums[i] = al.get(i);
         }
-        // for (int i = 0; i < al.size(); i++){
-        //     if (al.get(i) == val){
-        //         al.remove(i);
-        //     }
-        //     return al.size();
-        // }
-        
-        return al.size();
+       return al.size();
     }
 }
-
-
-// List<Integer> al = new ArrayList<>();
-//         for (int i : nums){
-//             al.add(i);
-//         }
-//         for (int i = 0; i < al.size(); i++){
-//             int count = 0;
-//             if (al.contains(val)){
-//                 al.remove(val);
-//                 count ++;
-//                 return count;
-//             }
-            
-//         }
-//         return al.size();
