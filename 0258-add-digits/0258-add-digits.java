772dfe301245ec82796ba302c510c1a1a60d7b3d
Ankita Soni <92040840+aankitasoni@users.noreply.github.com>
2023-05-30 22:27:50 +0530

    Time: 1 ms (100.00%), Space: 39.9 MB (55.99%) - LeetHub

--- 0258-add-digits/0258-add-digits.java
+++ 0258-add-digits/0258-add-digits.java
@@ -1,7 +1,7 @@
 class Solution {
     public int addDigits(int num) {
          if( num % 9 == 0 && num != 0 )
-            return 9;       // %9 --> want single digit
+            return 9;        // %9 --> want single digit
         if( num > 9)
             return num % 9;   
         return num;
