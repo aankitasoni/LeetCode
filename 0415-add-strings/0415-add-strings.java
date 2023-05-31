727b82c5c4f5d69c203a32d695a0dab710fdab2b
Ankita Soni <92040840+aankitasoni@users.noreply.github.com>
2023-06-01 00:42:10 +0530

    Time: 19 ms (16.10%), Space: 43.8 MB (8.03%) - LeetHub

--- 0415-add-strings/0415-add-strings.java
+++ 0415-add-strings/0415-add-strings.java
@@ -1,8 +1,8 @@
 import java.math.*;
 class Solution {
     public String addStrings(String num1, String num2) {
-        BigInteger a = new BigInteger(num1, 10); 
-        BigInteger b = new BigInteger(num2, 10);
+        BigInteger a = new BigInteger(num1); 
+        BigInteger b = new BigInteger(num2);
         BigInteger c = a.add(b);
         return c.toString(10);
     }
