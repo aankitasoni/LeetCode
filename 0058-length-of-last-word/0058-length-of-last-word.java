47d68a9d91d63f77bdf1022cc3aea875857036fa
Ankita Soni <92040840+aankitasoni@users.noreply.github.com>
2023-06-22 23:56:18 +0530

    Time: 0 ms (100.00%), Space: 40.3 MB (99.75%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,10 +1,16 @@
 class Solution {
     public int lengthOfLastWord(String s) {
         String ls = s.trim();
-       int i = ls.lastIndexOf(' ') +1;
-        return (ls.length() - i);
+        int count = 0;
+        for (int i = ls.length()-1 ; i >= 0; i--){
+            if (ls.charAt(i) == ' '){
+                break;
+            } else {
+                count++;
+            }
+        }
+        return count;
+        
         
     }
 }
-
-
