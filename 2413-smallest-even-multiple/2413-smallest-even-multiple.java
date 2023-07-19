ea5f5dde833eb24735b95370b3aa3505894b1432
Ankita Soni <92040840+aankitasoni@users.noreply.github.com>
2023-07-20 01:34:16 +0530

    Time: 0 ms (100.00%), Space: 39.5 MB (50.79%) - LeetHub

--- 2413-smallest-even-multiple/2413-smallest-even-multiple.java
+++ 2413-smallest-even-multiple/2413-smallest-even-multiple.java
@@ -1,9 +1,14 @@
 class Solution {
     public int smallestEvenMultiple(int n) {
-        if (n %2 == 0){
-            return n;
+//         if (n %2 == 0){
+//             return n;
+//         } 
+//         return n*2;
+            
+       if ((n & 1) == 0){
+           return n;
         } 
         return n*2;
-            
-    }
+       }
 }
+
