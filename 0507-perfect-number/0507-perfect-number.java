89a8254f6719945f8922b0f3b237bc9d81d66aa8
Ankita Soni <92040840+aankitasoni@users.noreply.github.com>
2023-08-02 01:44:29 +0530

    Time: 2089 ms (5.18%), Space: 39.1 MB (78.55%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.java
+++ 0507-perfect-number/0507-perfect-number.java
@@ -1,12 +1,12 @@
 class Solution {
     public boolean checkPerfectNumber(int num) {   
-        int sum = 0, n = num;
+        int sum = 0;
         for (int i = 1; i < num; i++){
             if (num % i == 0){
                 sum += i;
             } 
         }
-         if (n == sum){
+         if (num == sum){
                 return true;
             }
         return false;
