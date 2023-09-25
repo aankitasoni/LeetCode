98668f90dbb0feeb044f265b1822c338262b874e
Ankita Soni <92040840+aankitasoni@users.noreply.github.com>
2023-09-25 20:24:17 +0530

    Time: 1 ms (100.00%), Space: 40.8 MB (42.11%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -2,14 +2,14 @@ class Solution {
     public char findTheDifference(String s, String t) {
             char[] c = s.toCharArray();
             char[] d = t.toCharArray();
-        int p = 0;
+        char p = 0;
         for (int i=0; i< c.length; i++){
             p -= c[i];
         }
         for (int i =0; i< d.length; i++){
             p += d[i];
         }
-        return (char)p;
+        return p;
 }
 }
 
