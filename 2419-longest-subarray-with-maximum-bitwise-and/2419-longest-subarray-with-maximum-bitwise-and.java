bec50afd3a550113254d5ba7892fa7cea448bf33
Ankita Soni <92040840+aankitasoni@users.noreply.github.com>
2023-10-02 16:32:09 +0530

    Time: 3 ms (83.64%), Space: 55.3 MB (87.27%) - LeetHub

---
+++ 2419-longest-subarray-with-maximum-bitwise-and/2419-longest-subarray-with-maximum-bitwise-and.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int longestSubarray(int[] nums) {
+        int max = 0;
+        for (int i =0; i< nums.length; i++){
+            max = Math.max(max, nums[i]);
+        }
+        int count = 1;
+        int max_length = 1;
+        for (int i =1; i < nums.length; i++){
+            if (max == nums[i] && nums[i] == nums[i-1]){
+                count++;
+            } else {
+                count = 1;
+                
+            }
+            max_length = Math.max(max_length, count);
+        }
+        
+        return max_length;
+    }
+}
