9e2193bcb34697fbd2e2f2e1641e12ab45234904
Ankita Soni <92040840+aankitasoni@users.noreply.github.com>
2023-10-10 16:52:52 +0530

    Time: 1 ms (96.70%), Space: 43.7 MB (59.63%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,18 +1,30 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        ArrayList<Integer> al = new ArrayList<>();
-        for (int i =0; i < nums.length; i++){
-            if (al.contains(nums[i])){
-                continue;
-            } else {
-                al.add(nums[i]);
+        int count = 0;
+		for (int i =1; i<nums.length; i++){
+			if(nums[count] != nums[i]){
+                nums[count +1] = nums[i];
+                count++;
             }
-        }
-        Arrays.fill(nums, -101);
-        for (int i =0; i < al.size() ; i++){
-            nums[i] = al.get(i);
-        }
-        
-        return al.size();
+		}
+		return count+1;
     }
 }
+
+
+
+
+//         ArrayList<Integer> al = new ArrayList<>();
+//         for (int i =0; i < nums.length; i++){
+//             if (al.contains(nums[i])){
+//                 continue;
+//             } else {
+//                 al.add(nums[i]);
+//             }
+//         }
+//         Arrays.fill(nums, -101);
+//         for (int i =0; i < al.size() ; i++){
+//             nums[i] = al.get(i);
+//         }
+        
+//         return al.size();
