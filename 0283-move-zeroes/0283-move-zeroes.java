2e1c219130d3d30c6db0f4a69bfa7775878875b8
Ankita Soni <92040840+aankitasoni@users.noreply.github.com>
2023-10-12 19:49:43 +0530

    Time: 2 ms (68.41%), Space: 44.7 MB (88.20%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,14 +1,36 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        ArrayList<Integer> al = new ArrayList<>();
-        for (int i : nums){
-            if (i != 0 ){
-                al.add(i);
-            } 
+        int j= -1;
+        for (int i = 0; i< nums.length; i++){
+            if (nums[i] == 0){
+                j = i;
+                break;
+            }
         }
-        Arrays.fill(nums, 0);
-        for (int i =0 ; i < al.size(); i++){
-            nums[i] = al.get(i);
+        if (j == -1) {
+            return;
         }
+        
+        for (int i=j+1; i <nums.length; i++){
+            if (nums[i] != 0){
+                int p = nums[i];
+                nums[i] = nums[j];
+                nums[j] = p;
+                j++;
+            }
+        }
+        return;
     }
 }
+
+
+ // ArrayList<Integer> al = new ArrayList<>();
+ //        for (int i : nums){
+ //            if (i != 0 ){
+ //                al.add(i);
+ //            } 
+ //        }
+ //        Arrays.fill(nums, 0);
+ //        for (int i =0 ; i < al.size(); i++){
+ //            nums[i] = al.get(i);
+ //        }
