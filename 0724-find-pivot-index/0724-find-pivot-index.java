006c75b5dfd3f73c978c9b949a6eb1fd1ce5a618
Ankita Soni <92040840+aankitasoni@users.noreply.github.com>
2023-10-20 00:28:02 +0530

    Time: 1 ms (99.04%), Space: 44.2 MB (32.35%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -1,22 +1,35 @@
 class Solution {
     public int pivotIndex(int[] nums) {
-        int pivot = 0;
-        while (pivot < nums.length){
-            int l = 0, r =0;
-            for (int i =0; i<pivot; i++){
-                l += nums[i];
-            }
-            for (int i = pivot+1; i<nums.length; i++){
-                r += nums[i];
-            }
-            if (l == r){
-                return pivot;
-            } else {
-                pivot++;
-            }
+        int t =0, l =0;
+        for (int i : nums){
+            t += i;
         }
+        for (int i =0; i<nums.length;l += nums[i++]){
+            
+            if (2*l == t - nums[i]){
+            return i;
+        }
+        }
+       
         return -1;
     }
 }
+
+// int pivot = 0;
+//         while (pivot < nums.length){
+//             int l = 0, r =0;
+//             for (int i =0; i<pivot; i++){
+//                 l += nums[i];
+//             }
+//             for (int i = pivot+1; i<nums.length; i++){
+//                 r += nums[i];
+//             }
+//             if (l == r){
+//                 return pivot;
+//             } else {
+//                 pivot++;
+//             }
+//         }
+//         return -1;
 
            
