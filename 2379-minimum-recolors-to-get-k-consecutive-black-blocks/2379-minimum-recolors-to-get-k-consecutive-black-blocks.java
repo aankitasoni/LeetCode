f013d253da1042448f85a43dd80308b3f34d20aa
Ankita Soni <92040840+aankitasoni@users.noreply.github.com>
2023-10-28 01:25:04 +0530

    Time: 1 ms (94.24%), Space: 40.2 MB (74.44%) - LeetHub

--- 2379-minimum-recolors-to-get-k-consecutive-black-blocks/2379-minimum-recolors-to-get-k-consecutive-black-blocks.java
+++ 2379-minimum-recolors-to-get-k-consecutive-black-blocks/2379-minimum-recolors-to-get-k-consecutive-black-blocks.java
@@ -2,22 +2,19 @@ class Solution {
     public int minimumRecolors(String blocks, int k) {
         int count =0, total =0, i = 0; 
         for (; i < k; i++){
-            if(sum(blocks.charAt(i))){
+            if(blocks.charAt(i) == 'B'){
                 count++;
             }
         }
         total = count;
         for (; i <blocks.length(); i++){
-            if (sum(blocks.charAt(i))) count++;
-            if(sum(blocks.charAt(i-k))) count--;
+            if ((blocks.charAt(i) == 'B')) count++;
+            if((blocks.charAt(i-k)) == 'B') count--;
             total = Math.max(total,count);
         }
         return k-total;
     }
-    public boolean sum(char c){
-        if (c == 'B' || c == 'A'){
-            return true;
-        }
-       return false; 
+   
     }
-}
+
+
