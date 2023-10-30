a2d1272655a7ac038e09d49232be754a23e5a815
Ankita Soni <92040840+aankitasoni@users.noreply.github.com>
2023-10-31 00:24:15 +0530

    Time: 6 ms (28.54%), Space: 40.4 MB (11.88%) - LeetHub

---
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String convertToTitle(int c) {
+       String s = "";
+        while (c-- > 0){
+           s = (char) (c%26 + 'A')+s;
+            c /= 26;
+        }
+        
+        return s;
+    }
+}
