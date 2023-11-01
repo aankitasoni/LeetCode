5577bd775dcef43901ebc2694aef4429286ff4f1
Ankita Soni <92040840+aankitasoni@users.noreply.github.com>
2023-11-02 00:27:20 +0530

    Time: 1 ms (89.65%), Space: 43.1 MB (93.10%) - LeetHub

--- 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.java
+++ 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.java
@@ -6,8 +6,8 @@ class Solution {
                 nums[i+1] = 0;
             }
         }
-        
-        for(int i=0, j=0; i<nums.length; i++){
+        int j=0;
+        for(int i=0; i<nums.length; i++){
             if(nums[i]!=0){
                 int temp = nums[i];
                 nums[i] = 0;
