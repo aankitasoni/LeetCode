d0870b6b8de5993deaf8312e0aebc0fb2f1dd389
Ankita Soni <92040840+aankitasoni@users.noreply.github.com>
2023-11-23 20:39:38 +0530

    Time: 1 ms (84.62%), Space: 43.3 MB (29.25%) - LeetHub

--- 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.java
+++ 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.java
@@ -1,16 +1,27 @@
 class Solution {
     public int findFinalValue(int[] nums, int o) {
-        for(int i =0; i<nums.length; i++){
+       int i =0;
+        while(i != nums.length){
             if(nums[i] == o){
+                i=0;
                 o = 2*o;
             } else{
-                for(int j = 0; j< nums.length; j++){
-                    if (nums[j] == o){
-                        o = 2*o;
-                    }
-                }
+                i++;
             }
         }
         return o;
     }
 }
+
+ // for(int i =0; i<nums.length; i++){
+ //            if(nums[i] == o){
+ //                o = 2*o;
+ //            } else{
+ //                for(int j = 0; j< nums.length; j++){
+ //                    if (nums[j] == o){
+ //                        o = 2*o;
+ //                    }
+ //                }
+ //            }
+ //        }
+ //        return o;
