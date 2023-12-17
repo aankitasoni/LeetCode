4041fb8aea83b96452e07461429e3c17e35b0b64
Ankita Soni <92040840+aankitasoni@users.noreply.github.com>
2023-12-17 12:41:22 +0530

    Time: 1 ms (11.18%), Space: 45.4 MB (11.84%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -1,13 +1,20 @@
 class Solution {
     public int search(int[] nums, int target) {
-         ArrayList<Integer> al = new ArrayList<>();
-        for (int i : nums){
-            al.add(i);
+        //  ArrayList<Integer> al = new ArrayList<>();
+        // for (int i : nums){
+        //     al.add(i);
+        //     }
+        //  if (al.contains(target)){
+        //         return al.indexOf(target);
+        //     } else {
+        //         return -1;
+        // }
+        for (int i = 0; i <nums.length; i++){
+            if (nums[i] == target){
+                return i;
             }
-         if (al.contains(target)){
-                return al.indexOf(target);
-            } else {
-                return -1;
         }
+        return -1;
     }
 }
+
