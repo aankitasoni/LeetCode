efafeb5b9556a2d756e1f259294997c64534d48f
Ankita Soni <92040840+aankitasoni@users.noreply.github.com>
2024-04-11 00:08:29 +0530

    Time: 4 ms (86.41%), Space: 43.4 MB (65.78%) - LeetHub

---
+++ 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] deckRevealedIncreasing(int[] deck) {
+        int n = deck.length;
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        for(int i = 0; i < n; i++) {
+            queue.offer(i);
+        }
+
+        Arrays.sort(deck);
+
+        int ans[] = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            ans[queue.poll()] = deck[i];
+
+            queue.offer(queue.poll());
+        }
+        return ans;
+    }
+}
